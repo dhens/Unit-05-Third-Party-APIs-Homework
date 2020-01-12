4d920c6a3e6903fc9155ee6a55c4f72f022e1a9e
dhens <dawsonhensel@gmail.com>
2020-01-12 15:33:44 -0800

    added save function for each textarea box which adds the text to localstorage, and also a get function that pulls data from localstorage and places it in the correct textarea box. also added clear schedules button which removes text from each textarea box and clears localStorage

--- assets/js/planner.js
+++ assets/js/planner.js
@@ -1,27 +1,46 @@
+// set id to savebtn or find a way to correlate save btn with its neighbor textarea values
 let timeDisplay = $("#currentDay");
-let bodyContent = $("#container");
-
-let currentDay = moment().format("dddd, MMMM Do ");
-let currentHour = parseInt(moment().format("h"));
-console.log("Current hour is: " + currentHour);
-
+let currentDay = moment().format("dddd, MMMM Do");
+// 24 hour clock time
+let currentHour = parseInt(moment().hour());
 // Display the current day at the top of the page
 timeDisplay.text(currentDay);
-
+// Number of rows
 let blockLength = $(".time-block").length;
-let currentBlock = $(".time-block");
-    
-        $(".description").each(function setBackgroundColor(index) {
-            let currentTimeValue = parseInt(moment($(this).attr("id"))._i);
-            console.log(currentTimeValue)
-            if (moment(currentTimeValue).isBefore(currentHour)) {
-                console.log(this);
+
+    renderLocalStorage();
+
+        $(".description").each(function() {
+            let currentBlockTimeVal = parseInt(moment($(this).attr("id"))._i);
+            if (moment(currentBlockTimeVal).isBefore(currentHour)) {
                 $(this).addClass("past");
             }
-            else if (moment(currentTimeValue).isSame(currentHour)) {
+            else if (moment(currentBlockTimeVal).isSame(currentHour)) {
                 $(this).addClass("present");
             }
-            else if (moment(currentTimeValue).isAfter(currentHour)) {
+            else if (moment(currentBlockTimeVal).isAfter(currentHour)) {
                 $(this).addClass("future");
             }
         });
+
+        $(".saveBtn").on("click", function() {
+            // $(".saveBtn").each(function() {
+            for(let i = 0; i < blockLength; i++) {
+                let currentID = $(".description").eq(i).attr("id");
+                let value = $(".description").eq(i).val().trim();
+                localStorage.setItem(currentID, value);
+            }
+        });
+
+        $(".clearSchedule").on("click", function() {
+            $(".description").empty();
+            localStorage.clear();
+        });
+
+
+        function renderLocalStorage() {
+            for (let i = 9, j = 0; i < 18; i++, j++) {
+                let blockInfo = (localStorage.getItem(i))
+                $(".description").eq(j).text(blockInfo)
+            }
+        }
